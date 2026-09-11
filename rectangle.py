59366029fe789a5c8d2fdba8e29eaece85c93794
salievaa63-png <salievaa63@gmail.com>
2026-09-11 14:33:07 +0400

    Implement Rectangle class

---
+++ rectangle.py
@@ -0,0 +1,9 @@
+python
+class Rectangle:
+def __init__(self, width: float, height: float):
+if width <= 0 or height <= 0:
+raise ValueError("Стороны должны быть положительными")
+self.width = width
+self.height = height
+def area(self) -> float:
+return self.width * self.height
