bd6d085088e31c68029d5c75e7ee9d59574096df
salievaa63-png <salievaa63@gmail.com>
2026-09-25 15:09:06 +0400

    Update laba2.py

--- laba2.py
+++ laba2.py
@@ -1,12 +1,17 @@
 import math
 
 a, b, h = -0.3, 0.7, 0.05
-x = a
-while x <= b + 1e-9: # Небольшая погрешность, чтобы точно попасть в 0.7
-    # Формула из задания 2
+
+steps = int(round((b - a) / h))
+
+print(f"{'x':>6} | {'y':>10}")
+print("-" * 19)
+
+for i in range(steps + 1):
+    x = a + i * h
+
     num = abs(math.sin(x) - math.cos(x))
     den = (math.sin(x**2))**3 + (math.cos(x**3))**2
     y = math.sqrt(num / den)
     
-    print(f"x = {x:.2f}, y = {y:.5f}")
-    x += h
+    print(f"{x:6.2f} | {y:10.5f}")
